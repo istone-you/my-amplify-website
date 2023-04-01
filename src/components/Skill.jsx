51f278fcefb83913eb032b2da953164f9769a6fc
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-01 12:40:19 +0000

    画像の大きさ修正

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -16,57 +16,63 @@ import MongoDBImg from '../images/MongoDB.png'
 const Skill = () => {
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', paddingTop: "50px" }}>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={AWSImg} height="100px"/>
-                <p>AWS</p>
+                <p><b>AWS</b></p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={JavaScriptImg} height="100px"/>
                 <p>JavaScript</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={ReactImg} height="100px"/>
                 <p>React</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={PythonImg} height="100px"/>
                 <p>Pyhton</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={DockerImg} height="100px"/>
                 <p>Docker</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={TerraformImg} height="100px"/>
-                <p>Terraform</p>
+                <p><b>Terraform</b></p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={AnsibleImg} height="100px"/>
                 <p>Ansible</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={GrafanaImg} height="100px"/>
-                <p>Grafana</p>
+                <p><b>Grafana</b></p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={PrometheusImg} height="100px"/>
-                <p>Prometheus</p>
+                <p><b>Prometheus</b></p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={FluentdImg} height="100px"/>
                 <p>Fluentd</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={OpenSearchImg} height="100px"/>
                 <p>OpenSearch</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={PostgreSQLImg} height="100px"/>
                 <p>PostgreSQL</p>
             </div>
-            <div style={{ flex: '1 0 25%', textAlign: 'center' }}>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
                 <img src={MongoDBImg} height="100px"/>
                 <p>MongoDB</p>
+            </div>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
+            </div>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
+            </div>
+            <div style={{ flex: '1 0 12.5%', textAlign: 'center' }}>
             </div>
         </div>
     )
