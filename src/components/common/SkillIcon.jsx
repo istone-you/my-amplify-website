b30a8e62bf6deea846c93a06c4fd9fef2164bd5f
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-04 12:02:26 +0000

    AWSのSkillに改行を追加

--- src/components/common/SkillIcon.jsx
+++ src/components/common/SkillIcon.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import useMedia from 'use-media';
 
 const SkillIcon = (prpps) => {
-    const isWide = useMedia({ minWidth: '768px' })
-    const width = isWide ? '25%' : '30%'
     return (
         <div style={{margin: "0 auto"}}>
             <div style={{ textAlign: 'center', margin: "15px" }}>
