5b492b98fd5c3d9190d4b2a33e931648144334cc
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-05 05:12:43 +0000

    aboutに前職を追加

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,13 +29,17 @@ const about = () => {
                 </div>
                 <div className="Comment">
                     <b>
-                        I am 26 years old, from Kagawa Prefecture. I graduated from Kansai University, Faculty of Law, and have been working as an engineer since March 2022. I mainly deal with infrastructure (AWS) in my work, but I also do programming as a hobby.
+                        I am 26 years old, from Kagawa Prefecture. I graduated from Kansai University, Faculty of Law, and have been working as an engineer since March 2022.
+                        Until then, I was a real estate salesman.<br />
+                        I mainly deal with infrastructure (AWS) in my work, but I also do programming as a hobby.<br />
                         As for other hobbies, I enjoy playing guitar, watching soccer games, and reading books.<br />
                         I also love my job as much as my hobbies, and I often find myself studying even on days off.<br />
                     </b>
                     <br />
                     <p style={{marginBottom: "50px"}}>
-                        香川県出身の26歳。関西大学法学部卒で、2022年3月よりエンジニアとして活動しております。業務ではインフラ(AWS)を主に扱っておりますが、趣味でプログラミングも行っています。<br />
+                        香川県出身の26歳。関西大学法学部卒で、2022年3月よりエンジニアとして活動しております。
+                        それまでは不動産の営業をしておりました。<br />
+                        業務ではインフラ(AWS)を主に扱っておりますが、趣味でプログラミングも行っています。<br />
                         その他の趣味としては、ギターを弾く事やサッカーを観戦する事、本を読む事です。<br />
                         仕事も趣味と同じくらい好きで、ついつい休日でも勉強してしまいます。
                     </p>
