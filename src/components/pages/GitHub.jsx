3eefc49262620f8278ccf24cf36776561648441e
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-05 03:46:25 +0000

    GitHubレポジトリの説明文変更

--- src/components/pages/GitHub.jsx
+++ src/components/pages/GitHub.jsx
@@ -19,7 +19,7 @@ const GitHub = () => {
           <h2>JavaScript</h2>
           <Repository
             title="my-amplify-website"
-            description="React.jsとGatsby.jsを使ったWebサイトです。こちらのサイトのソースコードです。"
+            description="React.jsとGatsby.jsを使ったWebサイトで、こちらのサイトのソースコードです。(改良中)"
             width={200}
             alt="React-Gatsby.jpg"
             src={GatsbyImg}
