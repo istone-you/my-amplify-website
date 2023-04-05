5bf43205d48118a5049979fcc31b35c714d8ba2e
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-05 00:59:40 +0000

    sitetitleの変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,6 +94,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo />
 
 export default IndexPage
